2d3353b4a8f801da0207abfba1ce0550afb8122e
Randal Vance Cunanan <randal@randalvance.net>
2022-12-04 10:29:37 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Diagram as Code Example Using Structurizr and C#
+## Running Locally
+1. Run the Structurizr On-Premise Docker Image
+  1. `docker run -it --rm -p 8080:8080 -v PATH:/usr/local/structurizr structurizr/onpremises`
+  1. Replace `PATH` with your path to a directory where structurizer can save its state
+1. Navigate to `localhost:8080`
+1. Login with default username (`structurizr`) and password (`password`)
+1. Create a new workspace.
+1. Go to Settings of the workspace
+1. Find the API Key and API Secret
+  1. Set environment variables
+    1. `STRUCTURIZR_API_KEY` - The API Key of your workspace
+    1. `STRUCTURIZR_API_SECRET` - The API Secret off your workspace
+1. Go to `src/MyDiagrams` and run `dotnet run` command
+1. Your workspace should have been updated now with the diagram defined in your C# code.
+1. For the first time, your diagram's layout will be a mess, you can edit the diagram in the browser and click Auto Layout (the magic wand icon), then save the diagram. Any subsequent run of this program will then respect the layout.
+
+## References
+1. [Structurizr Getting Started](https://structurizr.com/share/18571/documentation#configuration)
+1. [Structurizr C# Client](https://github.com/structurizr/dotnet/blob/master/docs/getting-started.md)
---
+++ src/MyDiagrams/Program.cs
@@ -0,0 +1,46 @@
+﻿using Structurizr;
+using Structurizr.Api;
+
+var workspace = new Workspace("My Workspace", "This is a model of my software system.");
+var model = workspace.Model;
+
+var user = model.AddPerson("User", "A user of my software system.");
+var softwareSystem = model.AddSoftwareSystem("Software System", "My software system.");
+user.Uses(softwareSystem, "Uses");
+
+var views = workspace.Views;
+var systemContextView = views.CreateSystemContextView(softwareSystem, "SystemContext", "Example of System Context Diagram");
+systemContextView.AddAllSoftwareSystems();
+systemContextView.AddAllPeople();
+
+var styles = views.Configuration.Styles;
+styles.Add(new ElementStyle(Tags.Person) { Shape = Shape.Person });
+styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
+
+var config = GetStructurizerConfig();
+
+var structurizrClient = new StructurizrClient(config.Url, config.ApiKey, config.ApiSecret);
+// If you want to save the generated diagram JSON to a file, set it here
+// structurizrClient.WorkspaceArchiveLocation = new DirectoryInfo("diagrams");
+
+structurizrClient.PutWorkspace(config.WorkspaceID, workspace);
+
+(string Url, string ApiKey, string ApiSecret, int WorkspaceID) GetStructurizerConfig()
+{
+  var url = "http://localhost:8080/api";
+  var apiKey = Environment.GetEnvironmentVariable("STRUCTURIZR_API_KEY");
+  var apiSecret = Environment.GetEnvironmentVariable("STRUCTURIZR_API_SECRET");
+  var workspaceID = Environment.GetEnvironmentVariable("STRUCTURIZR_WORKSPACE_ID");
+
+  var missingEnvironmentVariables = new List<string>();
+  if (apiKey is null) missingEnvironmentVariables.Add("STRUCTURIZR_API_KEY");
+  if (apiSecret is null) missingEnvironmentVariables.Add("STRUCTURIZR_API_SECRET");
+  if (workspaceID is null) missingEnvironmentVariables.Add("STRUCTURIZR_WORKSPACE_ID");
+
+  if (missingEnvironmentVariables.Any())
+  {
+    throw new Exception($"Missing environment variables: {string.Join(", ", missingEnvironmentVariables)}");
+  }
+
+  return (url!, apiKey!, apiSecret!, Int32.Parse(workspaceID!));
+}
